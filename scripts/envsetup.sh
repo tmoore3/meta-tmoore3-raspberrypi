d1683b50e3dd572ea4507441b91fbb612564d8f9
Tom Moore <tomwmoore@hotmail.com>
2024-04-21 15:57:53 +0100

    Updated envsetup.sh to set MACHINE variable.

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -14,3 +14,11 @@ TEMPLATECONF=$CWD/sources/meta-tmoore3-raspberrypi/conf/templates
 OEROOT=$CWD/sources/poky
 
 . $OEROOT/oe-init-build-env $CWD/$BUILD_DIR
+
+# Set MACHINE in local.conf file
+if [ -z "$(grep '^MACHINE =' conf/local.conf)" ]; then
+    # Apply selected MACHINE in local conf file
+    sed -e 's/^\(MACHINE.*\)$/#\1\nMACHINE = "'"$MACHINE"'"/' -i conf/local.conf
+else
+    echo "[WARNING] MACHINE is already set in local.conf. Nothing done..."
+fi
